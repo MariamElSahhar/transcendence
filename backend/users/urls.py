bd8d536de3962272ff56718981bcca4a1347d355
MariamElSahhar <me1693@nyu.edu>
2024-10-06 22:53:18 +0400

    jwt backend enpoints

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from .views import (
     user_list_create_view,
     user_retrieve_update_destroy_view,
-    login_view,
     register_view,
-    logout_view,
 )
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -17,11 +15,7 @@ urlpatterns = [
         name="user-retrieve-update-destroy",
     ),
     # Authentication
-    path("login/", login_view, name="login"),
     path("register/", register_view, name="register"),
-    path("logout/", logout_view, name="logout"),
-
-    # # JWT
-    # path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    # path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,12 +1,13 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import UserSerializer, LoginSerializer
 from django.contrib.auth import authenticate
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.hashers import make_password
-
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Get users and create new user
 @api_view(["GET", "POST"])
@@ -19,7 +20,7 @@ def user_list_create_view(request):
     elif request.method == "POST":
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(password=make_password(serializer.validated_data['password']))
+        serializer.save(password=make_password(serializer.validated_data["password"]))
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
@@ -35,7 +36,7 @@ def user_retrieve_update_destroy_view(request, user_id):
     elif request.method == "PUT":
         serializer = UserSerializer(user, data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(password=make_password(serializer.validated_data['password']))
+        serializer.save(password=make_password(serializer.validated_data["password"]))
         return Response(serializer.data)
 
     elif request.method == "DELETE":
@@ -43,39 +44,41 @@ def user_retrieve_update_destroy_view(request, user_id):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# Login
-@api_view(["POST"])
-def login_view(request):
-    serializer = LoginSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
+# # Login
+# @api_view(["POST"])
+# def login_view(request):
+#     serializer = LoginSerializer(data=request.data)
+#     serializer.is_valid(raise_exception=True)
 
-    username = serializer.validated_data["username"]
-    password = serializer.validated_data["password"]
+#     username = serializer.validated_data["username"]
+#     password = serializer.validated_data["password"]
 
-    user = authenticate(username=username, password=password)
-    print(username, password, user)
-    if user is not None:
-        return Response({"detail": "Login successful"}, status=status.HTTP_200_OK)
-    else:
-        return Response(
-            {"detail": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
-        )
+#     user = authenticate(username=username, password=password)
+#     print(username, password, user)
+#     if user is not None:
+#         return Response({"detail": "Login successful"}, status=status.HTTP_200_OK)
+#     else:
+#         return Response(
+#             {"detail": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+#         )
 
 
 # Register
 @api_view(["POST"])
+@permission_classes([AllowAny])
 def register_view(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save(password=make_password(serializer.validated_data['password']))
+        serializer.save(password=make_password(serializer.validated_data["password"]))
+        user = CustomUser.objects.get(username=serializer.validated_data["username"])
+        refresh = RefreshToken.for_user(user)
         return Response(
-            {"detail": "User registered successfully!"}, status=status.HTTP_201_CREATED
+            {
+                "detail": "User registered successfully!",
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+            },
+            status=status.HTTP_201_CREATED,
         )
     else:
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# Logout
-@api_view(["POST"])
-def logout_view(request):
-    return Response({})
