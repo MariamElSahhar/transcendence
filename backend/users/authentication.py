4b4776de4e3dfe4da4b2f83a2a548c0455a636fd
MIkamal88 <109902742+MIkamal88@users.noreply.github.com>
2024-10-17 00:58:17 +0400

    Added DRF_Spectacular for API Schema and autoDoc

--- backend/users/authentication.py
+++ backend/users/authentication.py
@@ -1,6 +1,7 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 
+
 class CustomJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
         token = request.COOKIES.get("access_token")
