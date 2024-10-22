c4ac7866691ccc20dc3c9790511509ccb9cc160a
MariamElSahhar <me1693@nyu.edu>
2024-10-22 21:46:05 +0400

    comment clients

--- frontend/js/clients/token-client.js
+++ frontend/js/clients/token-client.js
@@ -1,37 +1,28 @@
 const BASE_URL = "http://127.0.0.1:8000/api";
 import { post, get } from "../utils/http-requests.js";
 
+// login and get new access and refresh tokens
 export const login = async ({ username, password }) => {
-	// login endpoint
 	const url = `${BASE_URL}/login/`;
 	const requestBody = { username, password };
-	// try sending request
 	const { status, body, error } = await post(url, requestBody);
-	// if there's an error
 	if (error) return { success: false, error: error };
-	// everything's fine - return data
 	return { success: true };
 };
 
+// get new access token
 export const refresh = async () => {
-	// token refresh endpoint
 	const url = `${BASE_URL}/token/refresh/`;
-	// try sending request
 	const { status, body, error } = await post(url);
-	// if there's an error
 	if (error) return { success: false, error: error };
-	// everything's fine - return data
 	return { success: true };
 };
 
+// register and get access and refresh tokens
 export const register = async ({ username, password }) => {
-	// registration endpoint
 	const url = `${BASE_URL}/register/`;
 	const requestBody = { username, password };
-	// try sending request
-	reponse = await post(url, credentials);
-	// if there's an error
+	reponse = await post(url, requestBody);
 	if (error) return { success: false, error: error };
-	// everything's fine - return data
 	return { success: true };
 };
