709233aa8f67ae068ad1dade72c575c5fb9cb273
MariamElSahhar <me1693@nyu.edu>
2024-10-24 12:35:10 +0400

    fix homepage

--- frontend/pages/HomePage.js
+++ frontend/pages/HomePage.js
@@ -1,11 +1,12 @@
 import { Component } from "./components/Component.js";
+import { isAuth } from "../../../js/clients/token-client.js";
 
 export class HomePage extends Component {
 	constructor() {
 		super();
 	}
 	render() {
-		if (userManagementClient.isAuth()) {
+		if (isAuth()) {
 			return `
       <navbar-component nav-active="home"></navbar-component>
       <friends-sidebar-component main-component="home-content-component"></friends-sidebar-component>
