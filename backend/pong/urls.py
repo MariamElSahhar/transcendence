d8daf8d54be3ca2e3782186b201b88a7b79aeb8b
MariamElSahhar <me1693@nyu.edu>
2024-12-17 09:49:36 +0400

    route api/remote-pong/ to remote-pong app

--- backend/pong/urls.py
+++ backend/pong/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/remote-pong/", include("remote_pong.urls")),
     path("api/", include("users.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/remote_pong/views.py
+++ backend/remote_pong/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+# TOKEN STATUS
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def test_view(request):
+    return Response({"message": "Hello from remote-pong api!"}, status=status.HTTP_200_OK)
+
