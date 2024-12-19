cfb450ae72dab88c10290fa603a8d6019e1c12fc
MariamElSahhar <me1693@nyu.edu>
2024-12-19 15:24:33 +0400

    empty error page

--- frontend/pages/error/ErrorPage.js
+++ frontend/pages/error/ErrorPage.js
@@ -1,26 +1,7 @@
 import { Component } from "../Component.js";
 
 export class ErrorPage extends Component {
-	static errorComponentName = "error-component";
-	static networkErrorMessage =
-		"Network error, " + "please check your network connection.";
-	static notFoundMessage = "Page not found";
-
-	static load(message, refresh = false) {
-		window.app.innerHTML = `
-        <${ErrorPage.errorComponentName}
-            message="${message}"
-            refresh="${refresh}">
-        </${ErrorPage.errorComponentName}>`;
-	}
-
-	static loadNetworkError() {
-		this.load(this.networkErrorMessage, true);
-	}
-
-	static loadNotFound() {
-		this.load(this.notFoundMessage, false);
-	}
+	// needs rewriting
 }
 
 customElements.define("error-page", ErrorPage);
