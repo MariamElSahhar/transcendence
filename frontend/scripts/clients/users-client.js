960ebddea791fae7276f0623d18c09ac34ddd1db
MariamElSahhar <me1693@nyu.edu>
2024-12-21 15:14:33 +0400

    comment unused clients

--- frontend/scripts/clients/users-client.js
+++ frontend/scripts/clients/users-client.js
@@ -53,22 +53,22 @@ export const deleteUser = async (id) => {
 	return { success: true, data: body };
 };
 
-export const usernameExist = async (username) => {
+/* export const usernameExist = async (username) => {
 	// console.log(username.username)
 	const url = `${URIs.users}/username/${username}/exists/`;
 	const { status, body, error } = await get(url);
 	if (error) return { success: false, error: error };
 	return { success: true, exists: body.exists };
-};
+}; */
 
-export const emailExist = async (email) => {
+/* export const emailExist = async (email) => {
 	// console.log(email.email)
 	const url = `${URIs.users}/email/${email}/exists/`;
 	const { status, body, error } = await get(url);
 	if (error) return { success: false, error: error };
 	return { success: true, body };
 };
-
+ */
 export const deleteAvatar = async ({ username }) => {
 	const url = `${URIs.users}/${username}/avatar/`;
 	const { status, body, error } = await del(url);
