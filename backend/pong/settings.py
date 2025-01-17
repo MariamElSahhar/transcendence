c4e542662cfafb2383b65f3c7bf3315b96211163
Anita Sulistiyawati <kunyit@Anitas-Mac-mini.local>
2025-01-17 17:59:53 +0400

    fix2

--- backend/pong/settings.py
+++ backend/pong/settings.py
@@ -157,7 +157,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=50000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": True,
