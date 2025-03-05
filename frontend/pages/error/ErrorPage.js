16ff6fe33521218c9b3aa9d77b4c2e9d054d86f2
Fathma Nazmeen <fathmanazmeen@MacBook-Pro.local>
2025-03-05 08:20:28 +0400

    error component redirection error

--- frontend/pages/error/ErrorPage.js
+++ frontend/pages/error/ErrorPage.js
@@ -8,11 +8,21 @@ export class ErrorPage extends Component {
 		this.dispatchEvent(
 			new CustomEvent("connected", { bubbles: true, composed: true })
 		);
+		const errorModal = document.getElementById("errorModal");
+		if (errorModal) {
+			const modal = new bootstrap.Modal(errorModal, {
+				backdrop: "static",
+				keyboard: false,
+			});
+			modal.show();
+		}
+		document.getElementById("refreshBtn").addEventListener("click", () => this.closeModal(window.location.pathname));
+		document.getElementById("homeBtn").addEventListener("click", () => this.closeModal("/home"));
 	}
 
 	render() {
 		return `
-		<div id="errorModal" class="modal fade show d-block" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
+		<div id="errorModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
 		<div class="modal-dialog modal-dialog-centered" role="document">
 		  <div class="modal-content">
 			<div class="modal-header">
@@ -23,7 +33,7 @@ export class ErrorPage extends Component {
 			  <p>Please try refreshing the page or go back to home.</p>
 			</div>
 			<div class="modal-footer">
-			  <button id="refreshBtn" class="btn btn-primary" onclick="window.redirect(${window.location.pathname})">Refresh Page</button>
+			  <button id="refreshBtn" class="btn btn-primary">Refresh Page</button>
 			  <button id="homeBtn" class="btn btn-primary" onclick="window.redirect('/home')">Home</button>
 			</div>
 		  </div>
@@ -32,20 +42,20 @@ export class ErrorPage extends Component {
 
 		`;
 	}
+	closeModal(pathname)
+	{
+		const modalElement = document.getElementById("errorModal");
+		const modal = bootstrap.Modal.getInstance(modalElement);
+		if (modal) {
+			modal.hide();
+		}
+		modalElement.remove();
+		window.redirect(pathname );
+	}
 }
-
 export async function showError() {
 	const errorPage = document.createElement("error-page");
 	document.body.appendChild(errorPage);
-	errorPage.addEventListener("connected", () => {
-		const errorModal = document.getElementById("errorModal");
-		if (errorModal) {
-			const modal = new bootstrap.Modal(errorModal, {
-				backdrop: "static",
-				keyboard: false,
-			});
-			modal.show();
-		}
-	});
+
 }
 customElements.define("error-page", ErrorPage);
